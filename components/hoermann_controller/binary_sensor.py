288af0db33e14d438b79b4c43d9c00ee4d0d17b7
Cory Redmond <cory.redmond@nandos.co.uk>
2025-09-30 20:14:10 +0100

    Added files...

--- components/hoermann_controller/binary_sensor.py
+++ components/hoermann_controller/binary_sensor.py
@@ -1,26 +1,32 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_TYPE
-from . import HoermannController
+from esphome.const import CONF_TYPE
 
-BINARY_SENSOR_TYPES = [
-    "error",
-    "prewarn",
-    "option_relay",
-]
+from . import hoermann_controller_ns, HoermannController
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
-    cv.Required("hoermann_controller_id"): cv.use_id(HoermannController),
-    cv.Required(CONF_TYPE): cv.enum(BINARY_SENSOR_TYPES, lower=True),
-})
+CONF_HOERMANN_CONTROLLER_ID = "hoermann_controller_id"
+
+# The cv.enum validator requires a dictionary
+BINARY_SENSOR_TYPES = {
+    "error": None,
+    "prewarn": None,
+    "option_relay": None,
+}
+
+CONFIG_SCHEMA = (
+    binary_sensor.binary_sensor_schema()
+    .extend(
+        {
+            cv.GenerateID(CONF_HOERMANN_CONTROLLER_ID): cv.use_id(HoermannController),
+            cv.Required(CONF_TYPE): cv.enum(BINARY_SENSOR_TYPES, lower=True),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
-    parent = await cg.get_variable(config["hoermann_controller_id"])
-    var = cg.new_P(config[CONF_ID])
-    await binary_sensor.register_binary_sensor(var, config)
-    
-    sensor_type = config[CONF_TYPE]
-    cg.add(parent.register_binary_sensor(sensor_type, var))
+    parent = await cg.get_variable(config[CONF_HOERMANN_CONTROLLER_ID])
+    var = await binary_sensor.new_binary_sensor(config)
+    cg.add(parent.register_binary_sensor(config[CONF_TYPE], var))
 
