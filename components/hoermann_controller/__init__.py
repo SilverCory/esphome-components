e45d3069a5cc293b5672f9cd6e25cddc20a19d82
Cory Redmond <cory.redmond@nandos.co.uk>
2025-09-30 20:19:43 +0100

    Added files...

--- components/hoermann_controller/__init__.py
+++ components/hoermann_controller/__init__.py
@@ -10,22 +10,29 @@ CONF_DE_PIN = "de_pin"
 hoermann_controller_ns = cg.esphome_ns.namespace("hoermann_controller")
 HoermannController = hoermann_controller_ns.class_("HoermannController", cg.Component, uart.UARTDevice)
 
-# Removed the cv.All wrapper to resolve the circular dependency error.
-CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(HoermannController),
-        cv.Required(CONF_DE_PIN): pins.gpio_output_pin_schema,
-    }
+# Restructured the schema to resolve the circular dependency.
+# By extending COMPONENT_SCHEMA first, we establish the base dependency,
+# then add the UART-specific parts.
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(HoermannController),
+            cv.Required(CONF_DE_PIN): pins.gpio_output_pin_schema,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
 )
+
 
 async def to_code(config):
     # Retrieve the component instance that was declared by the schema
     var = await cg.get_variable(config[CONF_ID])
-    
+
     # The component is already registered by the schema, so we just need to
     # register it as a UART device.
     await uart.register_uart_device(var, config)
-    
+
     de_pin = await cg.gpio_pin_expression(config[CONF_DE_PIN])
     cg.add(var.set_de_pin(de_pin))
 
